fd48f60270f50ab77d0e3f8ef94ff38bff714589
shubham947222 <shubham947222@gmail.com>
2021-06-03 10:21:56 +0530

    finishing sihn in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './components/pages/homepage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component';
 import SignInAndSignUpPage from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';   
+import { auth, createUserProfileDocument,userAuth} from './firebase/firebase.utils';   
 
 
 class App extends React.Component {
@@ -20,10 +20,23 @@ class App extends React.Component {
 
   unsubcribeFromAuth = null;
   componentDidMount() {
-    this.unsubcribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
-    });
+    this.unsubcribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          console.log(snapShot);
+          this.setState({
+            currentUser:{
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+          
+        })
+      }
+      else
+      this.setState({currentUser: userAuth})
+      });
   }
 
   componentWillUnmount() {
